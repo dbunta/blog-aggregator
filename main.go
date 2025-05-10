f3e6cacdd2a941960bd74488cda0b28d39dec606
dbunta <dmbunta@gmail.com>
2025-05-10 09:21:46 -0400

    add register command

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"os"
+	"time"
 
 	config "github.com/dbunta/blog-aggregator/internal/config"
+	"github.com/dbunta/blog-aggregator/internal/database"
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -13,12 +19,18 @@ func main() {
 		fmt.Println(fmt.Errorf("Error getting config: %w", err))
 		os.Exit(1)
 	}
+
+	db, err := sql.Open("postgres", c.DbUrl)
+	dbQueries := database.New(db)
+
 	var st state
 	st.config = &c
+	st.db = dbQueries
 
 	var cmds commands
 	cmds.handlers = make(map[string]func(*state, command) error)
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	args := os.Args
 	if len(args) < 2 {
@@ -43,6 +55,7 @@ func main() {
 
 type state struct {
 	config *config.Config
+	db     *database.Queries
 }
 
 type command struct {
@@ -75,10 +88,46 @@ func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		return fmt.Errorf("login error: no username provided in args")
 	}
-	err := s.config.SetUser(cmd.args[0])
+
+	user, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("login error: user does not exist")
+	}
+
+	err = s.config.SetUser(user.Name)
 	if len(cmd.args) == 0 {
 		return fmt.Errorf("login error: %w", err)
 	}
 	fmt.Printf("Current user has been set to %v\n", cmd.args[0])
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("register error: no name provided in args")
+	}
+	params := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.args[0],
+	}
+
+	_, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err == nil {
+		return fmt.Errorf("register error: user with that name already exists: %w", err)
+	}
+
+	newUser, err := s.db.CreateUser(context.Background(), params)
+	if err != nil {
+		return fmt.Errorf("register error: error creating user: %w", err)
+	}
+
+	err = s.config.SetUser(newUser.Name)
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("login error: %w", err)
+	}
+	fmt.Printf("new user %v was created\n", newUser.Name)
+	fmt.Printf("new user info: %v\n", newUser)
 	return nil
 }
